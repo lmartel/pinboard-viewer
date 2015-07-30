bf23f867e8f1997fd1e95e6ee514cc2c24c66646
Leo Martel <lmartel@github>
2015-07-30 15:54:57 -0400

    added securely stored secret token

--- js/common.js
+++ js/common.js
@@ -1,16 +1,17 @@
 // userInfo: name, pwd, isChecked
-var _userInfo = null, _tags = [], keyPrefix = 'pbuinfo',
-checkedkey = keyPrefix + 'c',
-namekey = keyPrefix + 'n', pwdkey = keyPrefix + 'p',
-authTokenKey = keyPrefix + '_auth_token',
+var _userInfo = null, _tags = [], keyPrefix = 'pb_viewer::',
+    checkedkey = keyPrefix + 'c',
+    namekey = keyPrefix + 'n',
+    pwdkey = keyPrefix + 'p',
+    authTokenKey = keyPrefix + 'auth_token',
 
-// dummy config option in the settings
-DUMMY_OPTION = keyPrefix + 'dummyoption',
+    // dummy config option in the settings
+    DUMMY_OPTION = keyPrefix + 'dummyoption',
 
-mainPath = 'https://api.pinboard.in/v1/',
+    mainPath = 'https://api.pinboard.in/v1/',
 
-yesIcon = '/img/icon_colored_19.png',
-noIcon = '/img/icon_grey_19.png',
-savingIcon = '/img/icon_grey_saving_19.png';
+    yesIcon = '/img/icon_colored_19.png',
+    noIcon = '/img/icon_grey_19.png',
+    savingIcon = '/img/icon_grey_saving_19.png';
 
 var REQ_TIME_OUT = 125 * 1000, maxDescLen = 500;
